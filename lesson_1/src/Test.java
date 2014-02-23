6c1fe8bc487917bbdf0942706f47aa8dac107984
Denis Stepulenok <super.denis@gmail.com>
2014-02-23 11:35:51 +0400

    Первый пример: Hello, groups 1893, 1894!

---
+++ lesson_1/src/Test.java
@@ -0,0 +1,23 @@
+/**
+ * Наша первая Java-программа
+ */
+public class Test {
+    public static void main(String args[]) {
+        // Pascal/Delphi: Writeln('Hello, groups 1893, 1894!');
+        // C: printf("Hello, groups 1893, 1894!\n");
+        // C++: cout << "Hello, groups 1893, 1894!" << endl;
+        /*
+          Многострочный...
+            ...комментарий
+        */
+        System.out.println("Hello, groups 1893, 1894!");
+
+        // Целочисленные типы:
+        int i = 10; // 32-битное целое
+        long l = 12L; // 64-битное целое
+
+        byte b = (byte) 0xff;
+        int bb = 0xff & b;
+
+    }
+}
