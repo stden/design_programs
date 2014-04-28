7a8a1eef8ee47727d6b4777abaf6cfc417385b5e
Denis Stepulenok <super.denis@gmail.com>
2014-04-29 02:08:55 +0400

    Другой способ вычисления

--- ArcSin/src/CalcTest.java
+++ ArcSin/src/CalcTest.java
@@ -13,26 +13,15 @@ public class CalcTest {
      */
     @Test
     public void factorial() {
-<<<<<<< HEAD
-        assertEquals(1, Calc.fact(1));
-        assertEquals(2, Calc.fact(2));
-        assertEquals(2 * 3, Calc.fact(3));
-        assertEquals(2 * 3 * 4, Calc.fact(4));
-=======
         assertEquals(1, Calc.fact(1), 0.0001);
         assertEquals(2, Calc.fact(2), 0.0001);
         assertEquals(2 * 3, Calc.fact(3), 0.0001);
         assertEquals(2 * 3 * 4, Calc.fact(4), 0.0001);
->>>>>>> origin/master
     }
 
     @Test
     public void zeroTest() {
-<<<<<<< HEAD
-        assertEquals(0.0, Calc.riad(0.0, 100), 1e-20);
-=======
         assertEquals(0.0, Calc.riad(0.0, 50), 1e-20);
->>>>>>> origin/master
     }
 
     /**
@@ -41,11 +30,7 @@ public class CalcTest {
     @Test
     public void linearTest() {
         for (double x = -0.3; x < 0.3; x += 0.001) {
-<<<<<<< HEAD
-            assertEquals(x, Calc.riad(x, 100), 0.01);
-=======
             assertEquals(x, Calc.riad(x, 80), 0.01);
->>>>>>> origin/master
         }
     }
 
@@ -54,13 +39,8 @@ public class CalcTest {
      */
     @Test
     public void checkPointTest() {
-<<<<<<< HEAD
-        assertEquals(PI / 2.0, Calc.riad(1, 2000), 0.01);
-        assertEquals(-PI / 2.0, Calc.riad(-1, 2000), 0.01);
-=======
         assertEquals(PI / 2.0, Calc.riad(1), 0.1);
         assertEquals(-PI / 2.0, Calc.riad(-1), 0.1);
->>>>>>> origin/master
     }
 
     /**
@@ -68,23 +48,15 @@ public class CalcTest {
      */
     @Test
     public void compareWithStandardImplementation() {
-<<<<<<< HEAD
         for (double x = 0; x < 1; x += 0.01) {
-            assertEquals("x = " + x, Math.asin(x), Calc.riad(x, 1000), 0.1);
+            assertEquals("x = " + x, Math.asin(x), Calc.riad(x), 0.1);
         }
     }
 
     @Test
     public void compareWithStandardImplementation2() {
         for (double x = 0; x < 1; x += 0.001) {
-            assertEquals("x = " + x, Math.asin(x), Calc.arcsin(x, 1000), 0.00001);
+            assertEquals("x = " + x, Math.asin(x), Calc.arcsin(x, 1000), 0.001);
         }
     }
-
-=======
-        for (double x = 0; x < 1; x += 0.1) {
-            assertEquals("x = " + x, Math.asin(x), Calc.riad(x), 0.1);
-        }
-    }
->>>>>>> origin/master
 }
