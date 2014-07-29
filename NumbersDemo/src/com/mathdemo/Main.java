c8139f3d7e104327153dd553f219631dab9e9ea0
Denis Stepulenok <super.denis@gmail.com>
2014-07-29 08:24:41 +0400

    Демонстрация точности вычислений

--- NumbersDemo/src/com/mathdemo/Main.java
+++ NumbersDemo/src/com/mathdemo/Main.java
@@ -1,25 +1,47 @@
 package com.mathdemo;
 
+import static java.lang.Math.abs;
+import static java.lang.System.out;
+
 public class Main {
 
+    /**
+     * Тесты
+     */
     public static void main(String[] args) {
-        check(0.3, 0.7, 1.0);
-        check(0.4, 0.7, 1.1);
-        check(0.5, 0.7, 1.2);
-        check(0.6, 0.7, 1.3);
-        check(0.7, 0.7, 1.4);
-        check(0.8, 0.7, 1.5);
+        sumDouble(0.4, 0.7, 1.1);
+        sumDouble(0.5, 0.7, 1.2);
+        sumFloat(0.5f, 0.7f, 1.2f);
+        sumDouble(0.6, 0.7, 1.3);
+        sumFloat(0.6f, 0.7f, 1.3f);
+        out.println("Но не думайте что float без потери точности...");
+        sumFloat(0.9f, 0.7f, 1.6f);
     }
 
-    /**
-     * Проверка, что a+b = c
-     */
-    static void check(double a, double b, double c) {
+    // Проверяем что a+b = c в double
+    static void sumDouble(double a, double b, double c) {
         if (a + b == c) {
-            System.out.printf("YES ;)  %s + %s == %s%n", a, b, c);
+            out.printf("YES ;)  %s + %s == %s - double", a, b, c);
         } else {
             double diff = a + b - c; // Разница
-            System.out.printf("NO :( %s + %s != %s (%s)%n", a, b, c, diff);
+            out.printf("NO :( %s + %s != %s (разница: %s) - double", a, b, c, diff);
         }
+        out.println("  equals(a+b,c) -> " + equals(a + b, c));
+    }
+
+    // Обработка погрешности
+    static boolean equals(double a, double b) {
+        return abs(a - b) < 1e-6; // Разность меньше EPS
+    }
+
+    // Проверяем что a+b = c в float
+    static void sumFloat(float a, float b, float c) {
+        if (a + b == c) {
+            out.printf("YES ;)  %s + %s == %s - float", a, b, c);
+        } else {
+            float diff = a + b - c; // Разница
+            out.printf("NO :( %s + %s != %s (разница: %s) - float", a, b, c, diff);
+        }
+        out.println("  equals(a+b,c) -> " + equals(a + b, c));
     }
 }
